101903fcf2348d6e2313af75b0d268e9784810ae
Gabriel <gabriel@pop-os.localdomain>
2022-06-07 16:36:07 -0300

    ajustes

--- capa/assets/js/scripts.js
+++ capa/assets/js/scripts.js
@@ -1,3 +1,139 @@
 setTimeout(function(){
   document.querySelector(".capa-container").style.overflow = "unset";
 }, 6000);
+
+document.querySelector(".link-capa-1").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-1").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-1").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
+
+document.querySelector(".link-capa-2").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-2").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-2").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+//
+
+document.querySelector(".link-capa-3").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-3").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-3").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
+
+document.querySelector(".link-capa-4").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-4").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-4").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
+
+document.querySelector(".link-capa-5").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-5").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-5").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
+
+document.querySelector(".link-capa-6").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-6").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-6").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
+
+document.querySelector(".link-capa-7").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-7").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-7").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
+
+document.querySelector(".link-capa-8").addEventListener("mouseover", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "0.3";
+  });
+  document.querySelector(".flor-8").style.opacity = "1";
+})
+
+document.querySelector(".link-capa-8").addEventListener("mouseout", function(){
+  var flores = document.querySelectorAll('.flor');
+  [].forEach.call(flores, function(flor) {
+    flor.style.opacity = "1";
+  });
+})
+
+// 
