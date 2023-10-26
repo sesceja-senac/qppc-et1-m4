a4279948c95f02e66b3c08645142b92371942576
FabianaRossatto <fmrossatto@senacrs.com.br>
2023-10-26 16:00:25 -0300

    .

--- paginado/template/js/geral.js
+++ paginado/template/js/geral.js
@@ -12,3 +12,33 @@ window.addEventListener('load', function(){
     })
 })
 
+var pg_number = parseInt(window.location.href.split("?")[1].split("=")[1]);
+console.log("pg_number:", pg_number);
+$("#navbarScroll a").eq(pg_number).addClass("ativo");
+$("#navbarScrollingDropdown span").html(
+  $("#navbarScroll a").eq(pg_number).text()
+);
+
+  //alert('URL: '+pg_number);
+
+  if (pg_number == 1) {
+    document.body.style.backgroundImage = "url('assets/pagina1/background.jpg')";
+  } else if (pg_number == 2) {
+    document.body.style.backgroundImage = "url('assets/pagina1/background.jpg')";
+    document
+      .getElementById("titulo-header")
+      .setAttribute("style", "color:#fff");
+  } else if (pg_number == 3) {
+    document.body.style.backgroundImage = "url('images/bg_pg3.jpg')";
+  } else if (pg_number == 4) {
+    document.body.style.backgroundImage = "url('images/bg_pg4.jpg')";
+    document
+      .getElementById("titulo-header")
+      .setAttribute("style", "color:#fff");
+  } else if (pg_number == 5) {
+    document.body.style.backgroundImage = "url('images/bg_pg5.jpg')";
+  } else if (pg_number == 6) {
+    document
+      .getElementById("titulo-header")
+      .setAttribute("style", "color:#fff");
+  }
